532f2f25e6e2d2e4536fb6be627d1b418582b490
S.Saha <soumabhasaha1509+github@gmail.com>
2025-05-06 01:00:22 +0530

    upgraded to vite-ssr-react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,27 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import AOS from "aos";
 import './App.css';
-import 'aos/dist/aos.css'
-import Home from './pages/Home.tsx';
-function AOSWrapper({ children }: { children: React.ReactNode }) {
-  const location = useLocation();
-  useEffect(() => {
-    AOS.init({
-      duration: 500,
-      once: true, // Animate only once per element
-    });
-    AOS.refresh(); // Refresh AOS on route change
-  }, [location.pathname]); // Re-run effect when the route changes
-  return <>{children}</>;
-}
 
 function App() {
-  // const [count] = useState(0);
+  // const [count, setCount] = useState(0)
+
   return (
-    <Routes>
-      <Route path="/" element={
-        <AOSWrapper>
-          <Home />
-        </AOSWrapper>
-      } />
-    </Routes>
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+          </button>
+        </div>
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default App;
+export default App
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  },
-  base: "./",
   plugins: [
     react(),
     tailwindcss()
   ],
-});
+})
