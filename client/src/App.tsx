f84dc38d801abdc67eaa4e6666afc33203f58f59
S.Saha <soumabhasaha1509+github@gmail.com>
2025-05-07 00:28:13 +0530

    ssr removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,29 @@
 import './App.css';
-
+import { Helmet } from 'react-helmet';
+import { IoMenu ,IoEllipsisHorizontal } from "react-icons/io5";
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>MarkItDown</title>
+      </Helmet>
       <div className="navbar bg-base-100 shadow-sm">
         <div className="flex-none">
           <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+            <IoMenu className='scale-150' />
           </button>
         </div>
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">MarkItDown</a>
         </div>
         <div className="flex-none">
           <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
+            <IoEllipsisHorizontal className='scale-150'/>
           </button>
         </div>
       </div>
     </>
   )
 }
-
-export default App
+export default App;
--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,40 +1,12 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Res, Req } from '@nestjs/common';
+import { Response, Request } from 'express';
 import { AppService } from './app.service';
-import { Request, Response } from 'express';
-import { promises as fs } from 'fs';
-import * as path from 'path';
-async function loadServerRenderer() {
-  try {
-    const { render } = await import(path.resolve(__dirname, '../../../client/dist/server/entry-server.js'));
-    // console.log(render)
-    return render;
-  } catch (error) {
-    console.error('Error loading server renderer:', error);
-    return null;
-  }
-}
-@Controller('*')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
   @Get()
-  async renderApp(@Req() req: Request, @Res() res: Response) {
-    const url = req.originalUrl;
-    const render = await loadServerRenderer();
-    if (!render)  return res.status(500).send('Server renderer not available.');
-    try {
-      const template = await fs.readFile(path.resolve(__dirname, '../../../client/dist/client/index.html'),'utf-8');
-      // You might need to pass a context object to your renderer
-      const context = {};
-      const appHtml = await render(url, context);
-      const html = template.replace(`<div id="root"></div>`, `<div id="root">${appHtml}</div>`);
-      // Inject any context data into the HTML if needed
-      // const finalHtml = html.replace('', JSON.stringify(context));
-      res.setHeader('Content-Type', 'text/html');
-      console.log(html)
-      res.send(html);
-    } catch (error) {
-      console.error('Rendering error:', error);
-      res.status(500).send('Internal Server Error');
-    }
+  getHello(): string {
+    return this.appService.getHello();
   }
+
 }
